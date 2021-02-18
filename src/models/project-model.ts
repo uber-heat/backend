4a1c7a0b47467ace713414133979c14c0f246ddf
Lelberto <jsurieux@gmail.com>
2021-02-18 12:26:05 +0100

    Add project model

---
+++ src/models/project-model.ts
@@ -0,0 +1,56 @@
+import mongooseToJson from '@meanie/mongoose-to-json';
+import { Mongoose, Model, Schema, Document } from 'mongoose';
+import ServiceContainer from '../services/service-container';
+import Attributes from './model';
+import { UserInstance } from './user-model';
+
+/**
+ * Project attributes interface.
+ */
+export interface ProjectAttributes extends Attributes {
+  owner: UserInstance;
+  name: string;
+  description: string;
+}
+
+/**
+ * Project instance interface.
+ */
+export interface ProjectInstance extends ProjectAttributes, Document {}
+
+/**
+ * Creates the project model.
+ * 
+ * @param container Services container
+ * @param mongoose Mongoose instance
+ */
+export default function createModel(container: ServiceContainer, mongoose: Mongoose): Model<ProjectInstance> {
+    return mongoose.model('Project', createProjectSchema(), 'projects');
+}
+
+/**
+ * Creates the project schema.
+ * 
+ * @returns Project schema
+ */
+function createProjectSchema() {
+  const schema = new Schema({
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Owner is required']
+    },
+    name: {
+      type: Schema.Types.String,
+      required: [true, 'Name is required']
+    },
+    description: {
+      type: Schema.Types.String,
+      default: null
+    }
+  });
+
+  schema.plugin(mongooseToJson);
+
+  return schema;
+}
